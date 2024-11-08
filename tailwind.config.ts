874cc25b08e02c9e7c0b6e2204502ef72263de7a
henryczup <140286297+henryczup@users.noreply.github.com>
2024-11-08 12:57:01 -0600

    Tailwind Config Fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,16 @@ const {
 	default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
+function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+
+	addBase({
+		":root": newVars,
+	});
+}
 
 const config = {
 	darkMode: ["class"],
@@ -18,7 +28,7 @@ const config = {
 	prefix: "",
 	theme: {
 		container: {
-			center: 'true',
+			center: true,
 			padding: '2rem',
 			screens: {
 				'2xl': '1400px'
@@ -81,48 +91,19 @@ const config = {
 					to: {
 						height: '0'
 					}
-				},
-				'accordion-down': {
-					from: {
-						height: '0'
-					},
-					to: {
-						height: 'var(--radix-accordion-content-height)'
-					}
-				},
-				'accordion-up': {
-					from: {
-						height: 'var(--radix-accordion-content-height)'
-					},
-					to: {
-						height: '0'
-					}
 				}
 			},
 			animation: {
-				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out',
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out'
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate"),
+	plugins: [
+		require("tailwindcss-animate"),
 		addVariablesForColors,
-	require('@tailwindcss/typography'),
+		require('@tailwindcss/typography')
 	],
 } satisfies Config
 
 export default config
-
-function addVariablesForColors({ addBase, theme }: any) {
-	let allColors = flattenColorPalette(theme("colors"));
-	let newVars = Object.fromEntries(
-		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-	);
-
-	addBase({
-		":root": newVars,
-	});
-}
-
