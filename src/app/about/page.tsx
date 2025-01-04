6edbb1d02e16eede3f5dd08b52fd73daeb4a0d4c
aadya-g <aadyaganjigunta@gmail.com>
2025-01-03 17:31:48 -0800

    import page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,22 @@
-import { Page } from "@/components/aboutus/aboutpage"
+import { ExecBoard } from "@/components/aboutus/execboard"
+import { XiChapter } from "@/components/aboutus/xichapter"
+import { Pillars } from "@/components/aboutus/pillars"
+import { OurHistory } from "@/components/aboutus/history"
+import Page from "@/components/aboutus/aboutpage";
 
 export default function AboutUs() {
     return (
         <div className="min-h-screen bg-background">
-                    {/* About*/}
-                    <Page />
-                </div>
-            )
-        }
+            {/* ExecBoard*/}
+                <ExecBoard />
+            {/* Xi Chapter*/}
+                <XiChapter />
+            {/* Pillars */}
+                <Pillars />
+            {/* Our History*/}
+                <OurHistory />
+        </div>
+
+    )
+}
         
