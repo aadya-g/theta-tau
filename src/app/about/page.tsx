ed53a06adcddae27e2cd54b93bafd07b2c32c7b3
aadya-g <aadyaganjigunta@gmail.com>
2025-01-03 16:48:47 -0800

    about app

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Page } from "@/components/aboutus/aboutpage"
+
+export default function AboutUs() {
+    return (
+        <div className="min-h-screen bg-background">
+                    {/* About*/}
+                    <Page />
+                </div>
+            )
+        }
+        
