5fbbfa4252d2c0a2957a9cfbdff1201f933db970
aadya-g <aadyaganjigunta@gmail.com>
2025-12-03 14:49:13 -0600

    Add new home page

--- src/components/new-home/home-blog-server.tsx
+++ src/components/new-home/home-blog-server.tsx
@@ -1,13 +1,20 @@
-// SERVER COMPONENT
-
 import { getSortedPostsData } from "@/lib/blog";
+import type { BlogPostsGridProps } from "../ui/blog-posts-grid";
 
-export default async function HomeBlogServer({ BlogPostsGrid }) {
+type ServerProps = {
+  BlogPostsGrid: React.ComponentType<BlogPostsGridProps>;
+};
+
+export default async function HomeBlogServer({ BlogPostsGrid }: ServerProps) {
   const allPosts = await getSortedPostsData();
   const recentPosts = allPosts.slice(0, 3);
 
-  // IMPORTANT: BlogPostsGrid is a FUNCTION, so call it properly
-  return <BlogPostsGrid posts={recentPosts} columns={3} showArrow={true} fixedHeight={true} />;
+  return (
+    <BlogPostsGrid
+      posts={recentPosts}
+      columns={3}
+      showArrow={true}
+      fixedHeight={true}
+    />
+  );
 }
-
-export type { BlogPostsGridProps };
